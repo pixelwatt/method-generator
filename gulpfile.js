6a7ea67fe22ae17ebb4ca0fafe76b134087ece81
Rob Clark <rob@pixelwatt.com>
2020-08-23 19:35:27 -0500

    Removed unused project dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,9 @@
 var gulp = require('gulp'),
-sass = require('gulp-dart-sass'),
-autoprefixer = require('gulp-autoprefixer'),
-jshint = require('gulp-jshint'),
-uglify = require('gulp-uglify'),
-imagemin = require('gulp-imagemin'),
-rename = require('gulp-rename'),
-concat = require('gulp-concat'),
 notify = require('gulp-notify'),
-cache = require('gulp-cache'),
-browserSync = require('browser-sync').create(),
 del = require('del'),
-watchSass = require("gulp-watch-sass"),
-cleanCSS = require('gulp-clean-css'),
 nunjucksRender = require('gulp-nunjucks-render');
 
-gulp.task('rebuild-sunrise', function() {
+gulp.task('rebuild-method', function() {
   return gulp.src('./njk/assets/**/*.*')
     .pipe(nunjucksRender({
       path: ['./njk/templates'],
